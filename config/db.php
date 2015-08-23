412ab3f4db451a9309a27682be27c19a18970b47
ftbl2631 <ftbl2631@qq.com>
2015-08-23 12:40:51 +0800

    comment

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=fub_db',
+    'username' => 'root',
+    'password' => '12345678oo',
+    'charset' => 'utf8',
+];
---
+++ views/site/regist.php
@@ -0,0 +1,22 @@
+<?php
+
+use yii\helpers\Html;
+use yii\bootstrap\ActiveForm;
+
+
+$this->title = '注册';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+
+<div class="site-regist">
+    <!--    -->
+        <h1><?= Html::encode($this->title) ?></h1>
+    
+  
+
+    <p>
+        This is the About page. You may modify the following file to customize its content:
+    </p>
+
+
+
